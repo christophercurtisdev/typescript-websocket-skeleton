9f3df463a6508b3cf1622b68cf772d28e5328920
Chris Curtis <curtischristopher0@gmail.com>
2024-10-21 23:18:40 +0100

    Cleaned up npm build scripts + added tailwind + lobby code input

--- typescript-client/src/main.ts
+++ typescript-client/src/main.ts
@@ -10,20 +10,21 @@ $(function() {
     var sendInstructionButton = $('#sendInstructionButton');
     var sendMessageButton = $('#sendMessageButton');
     initialiseLobbyButtonListeners();
-    initialiseReactorInterface();
 
     function initialiseLobbyButtonListeners() {
         createLobbyButton.on('click', function() {
             reactorSocket.sendLobbyRequest({ request: 'create' }); // Send a lobby request object
+            $('body').load('controller.html');
         });
 
         joinLobbyButton.on('click', function() {
-            reactorSocket.sendLobbyRequest({ request: 'join', lobbyCode: 'ABC123' }); // Send a lobby request object
+            console.log($('input#lobbyCode').val());
+            if ($('input#lobbyCode').val()) {
+                let lobbyCode = $('input#lobbyCode').val() as string;
+                reactorSocket.sendLobbyRequest({ request: 'join', lobbyCode: lobbyCode }); // Send a lobby request object
+                $('body').load('inspector.html');
+            }
         });
-    }
-
-    function initialiseReactorInterface() {
-
     }
 
     function initialiseControllerListeners() {
