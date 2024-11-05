8a770c36d78214d99ce9e6437135c05a9bb0a896
Chris Curtis <curtischristopher0@gmail.com>
2024-11-05 23:56:54 +0000

    Multiple draggables + sent terminal message to server

--- typescript-client/src/main.ts
+++ typescript-client/src/main.ts
@@ -4,22 +4,24 @@ import interact from "interactjs";
 
 $(function () {
 
-    const position = { x: 0, y: 0 }
+    let positions: any = [];
 
     interact('.draggable').draggable({
         ignoreFrom: '.switch',
         listeners: {
             move(event) {
-                position.x += event.dx
-                position.y += event.dy
+                positions[event.target.id] = positions[event.target.id] ?? { x: 0, y: 0 }
+                let position: any = positions[event.target.id];
+                position.x += event.dx;
+                position.y += event.dy;
 
                 event.target.style.transform =
-                    `translate(${position.x}px, ${position.y}px)`
-            },
+                    `translate(${position.x}px, ${position.y}px)`;
+            }
         }
     });
 
-    const reactorSocket = new SimpleSocket('ws://localhost:8082', updateInspectorValues);
+    const reactorSocket = new SimpleSocket('ws://localhost:8082', onMessage);
 
     var createLobbyButton = $('#createLobbyButton');
     var joinLobbyButton = $('#joinLobbyButton');
@@ -28,8 +30,9 @@ $(function () {
     function initialiseLobbyButtonListeners() {
         createLobbyButton.on('click', function () {
             reactorSocket.sendLobbyRequest({ request: 'create' }); // Send a lobby request object
-            $('body').load('controller.html', function (response, status) {
+            $('#main').load('controller.html', function (response, status) {
                 initialiseControllerListeners();
+                $('input#playerType').val('contorller');
             });
         });
 
@@ -37,7 +40,10 @@ $(function () {
             if ($('input#lobbyCode').val()) {
                 let lobbyCode = $('input#lobbyCode').val() as string;
                 reactorSocket.sendLobbyRequest({ request: 'join', lobbyCode: lobbyCode }); // Send a lobby request object
-                $('body').load('inspector.html');
+                $('#main').load('inspector.html', function (response, status) {
+                    initialiseStatsListeners();
+                    $('input#playerType').val('inspector');
+                });
             }
         });
     }
@@ -45,7 +51,7 @@ $(function () {
     function initialiseControllerListeners() {
         $('input').on('change', function () {
             let board = new Map();
-            $('input[type=checkbox]').each(function(index) {
+            $('input[type=checkbox]').each(function (index) {
                 board.set($(this).attr('id'), $(this).prop('checked'));
             });
             reactorSocket.sendBoardData({ board: board }); // Send an instruction object
@@ -53,9 +59,23 @@ $(function () {
     }
 
     function initialiseStatsListeners() {
-        // sendMessageButton.on('click', function () {
-        //     reactorSocket.sendMessage({ value: 'pressed' }); // Send a message object
-        // });
+        $(document).on('keypress', function (e) {
+            if (e.which == 13) {
+                let message = $('#terminalInput').val() as string;
+                $('#terminalInput').val('');
+                reactorSocket.sendMessage({ value: message }); // Send a message object
+            }
+        });
+    }
+
+    function onMessage(response: MessageEvent<any>) {
+        if ($('input#playerType').val() == 'controller') {
+            updateControllerValues(response);
+        }
+
+        if ($('input#playerType').val() == 'inspector') {
+            updateInspectorValues(response);
+        }
     }
 
     function updateInspectorValues(response: MessageEvent<any>) {
