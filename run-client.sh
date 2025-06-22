aa84b34af28bbf59941d3e42983ddf63ce4b49aa
Chris Curtis <curtischristopher0@gmail.com>
2025-06-22 17:03:11 +0100

    Added tmuxinator and removed run scripts that didn't work

--- run-client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/bin/sh -ec 'cd typescript-client && npm run build'
-/bin/sh -ec 'cd typescript-client && live-server public'
--- run-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/bin/sh -ec 'cd typescript-server && tsc -b'
-/bin/sh -ec 'cd typescript-server && node dist/index.js'
